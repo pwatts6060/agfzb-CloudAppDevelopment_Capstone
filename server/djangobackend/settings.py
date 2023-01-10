9a69fe615437b3798145a1de09fa1a38c7548994
Patrick <patrick.watts@hotmail.co.uk>
2023-01-10 16:41:01 +0400

    update deployment files for module 5

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "djangoapp.mybluemix.net", "https://patrickwatts-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/djangoapp"]
 
 
 # Application definition
